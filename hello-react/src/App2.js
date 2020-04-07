f4e9817bba9a32d45397ab91361f08aa5c1c978a
Zagoshipda <john713@hanmail.net>
2020-04-07 18:23:07 +0900

    initial commit

---
+++ hello-react/src/App2.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App2(){
+    state = {
+        curr : new Date().toLocaleTimeString()
+    }
+    componentDidMount(){
+        setInterval(updateTime, 1000);
+    }
+
+    updateTime = () => {
+        this.setState({
+            curr : new Date().toLocaleTimeString()
+        });
+    }
+
+    return (
+        <div>
+            <h1>It is {this.curr}. </h1>
+        </div>
+    );
+}
+
+export default App2;
